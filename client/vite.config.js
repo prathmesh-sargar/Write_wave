9b12df78cc169171204e3c6d91d73cd122298137
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2025-06-29 13:14:31 +0530

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://write-wave-v38z.onrender.com',
         secure: false,
       },
     },
