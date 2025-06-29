5841ccddf98c185f32fefd9aa7c4c7116d7d3cb6
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2025-06-29 13:21:01 +0530

    added changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ important for Vercel to serve assets correctly
   server: {
     proxy: {
       '/api': {
