6fcc089bd8fbe3b48d6ba62bb5d3dc0085717e64
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2025-06-29 13:27:34 +0530

    remove proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // ✅ important for Vercel to serve assets correctly
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://write-wave-v38z.onrender.com',
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
